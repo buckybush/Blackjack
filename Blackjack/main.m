577304db8079f3ec224e56eaf53fd409975e1d68
Bucky <buckybush@gmail.com>
2011-07-26 12:47:50 -0400

    functional single deals except split

--- Blackjack/main.m
+++ Blackjack/main.m
@@ -9,6 +9,10 @@ int main (int argc, const char * argv[])
     Dealer *frank = [[Dealer alloc]init];
     int hand = 0;
     int card;
+    int stand = 0;
+    int userInput;
+    int bust = 0;
+    int blackjack = 0;
     
     // Player Deal
     card=[frank dealCard:hand]; // Deal card 1
@@ -18,6 +22,9 @@ int main (int argc, const char * argv[])
     hand+=card;
     NSLog(@"Card 2: %i",card);
     NSLog(@"Hand = %i",hand);
+    if (hand==21) {
+        blackjack=1;
+    }
 
     NSLog(@"\n"); //Break
     
@@ -27,16 +34,14 @@ int main (int argc, const char * argv[])
     
     NSLog(@"\n"); //Break
     
-    int stand = 0;
-    int userInput;
-    int bust = 0;
+   
     
-    while(!stand && !bust){
+    while(!stand && !bust && !blackjack){
         userInput=-1;
         
         // User Input
         NSLog(@"1: Hit");
-        NSLog(@"2: Split");
+        NSLog(@"2: Split (not working)");
         NSLog(@"3: Stand");
         NSLog(@"0: Quit");
         printf("Your turn: ");
@@ -50,11 +55,14 @@ int main (int argc, const char * argv[])
             if (hand>21) {
                 NSLog(@"BUST!");
                 bust = 1;
+            } else if (hand==21){
+                blackjack=1;
+            } else{
             }
         }else if(userInput==2){
             //Split
         }else if(userInput==3){
-        
+            stand=1;
         }else {
             NSLog(@"\nPlease enter a valid input\n");
             userInput= -1;
@@ -64,22 +72,24 @@ int main (int argc, const char * argv[])
     
     
     if(!bust){
-    // Dealer selfDeal Cards 2-n
-    frank.dealerHand=[frank selfDeal:frank.dealerHand];
+        // Dealer selfDeal Cards 2-n
+        frank.dealerHand=[frank selfDeal:frank.dealerHand];
+        NSLog(@"You have: %i",hand);
     } else {
         NSLog(@"You busted...");
     }
     
+    
     if(!bust){
         // Who wins?
-        if (frank.dealerBust && !bust) {
+        if (frank.dealerBust) {
             NSLog(@"The dealer busted, YOU WIN!");
-        }else if (hand>frank.dealerHand && !bust){
+        }else if (hand>frank.dealerHand){
             NSLog(@"You win!");
-        }else if (hand<frank.dealerHand){
+        }else if (hand<=frank.dealerHand){
             NSLog(@"Dealer wins");
-        }else {
-            NSLog(@"else");
+        }else if (blackjack){
+            NSLog(@"Blackjack!");
         }
     }
     
